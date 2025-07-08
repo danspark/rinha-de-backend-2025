2b610e6a732b34625828d0e07127feb42b0d6ea1
Daniel Reis <dddanielreis@proton.me>
2025-07-08 14:22:09 -0300

    Add Docker Compose configuration and PostgreSQL integration

--- src/apphost/AppHost.cs
+++ src/apphost/AppHost.cs
@@ -3,12 +3,22 @@ var builder = DistributedApplication.CreateBuilder(args);
 const int apiPort = 9999;
 const string paymentProcessorNetwork = "payment-processor";
 
+var db = builder.AddPostgres("db-server")
+    .WithImage("dddanielreis/orleans-postgres")
+    .WithPgAdmin()
+    .AddDatabase("db");
+
 builder.AddProject<Projects.Api>("api", configure: options =>
     {
         options.ExcludeLaunchProfile = true;
     })
+    .WithReference(db)
+    .WaitFor(db)
     .WithHttpEndpoint(apiPort)
-    .PublishAsDockerComposeService((_, s) => s.Networks.Add(paymentProcessorNetwork));
+    .PublishAsDockerComposeService((_, s) =>
+    {
+        s.Networks.Add(paymentProcessorNetwork);
+    });
 
 builder.AddDockerComposeEnvironment("docker-compose")
     .ConfigureComposeFile(compose =>
